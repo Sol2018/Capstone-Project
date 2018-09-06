5446e30efac3324da7a14e7f26d6fe80790999d7
mlungiseleli-notshokovu <mlungiselelinotshokovu@gmail.com>
2018-09-06 02:47:25 +0200

    Update search functionality

--- script.js
+++ script.js
@@ -1,35 +1,70 @@
-//variable to store the search input value
-var radioValue = '';
+//variable to store the search input value and radio box
+var radioValue;
 var searchInput;
-//add action listener when the search button is clicked
-document.getElementById('search-btn').addEventListener('click', checkValue);
-var radioChecked = false; //for the checked radio button, initialized to false
+var ourData;
+//to keep track of the current page and where to add data
+var currentPage = document.querySelector('#showcase').innerHTML.toLowerCase().trim();
+//add action listener when the search button is clicked and invokes the checkValue() function
+document.getElementById('search').addEventListener('click', searchFile);
 
-function checkValue() {
 
-    var files = document.getElementsByName('file'); //array to store all file names
 
-    searchInput = document.getElementById('search-input').value; //the input value
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', '/sample.json');
+ourRequest.onload = function () {
+    ourData = JSON.parse(ourRequest.responseText);
+    localStorage.setItem('files', JSON.stringify(ourData));
+    //alert(ourData[0].title);
+};
+ourRequest.send();
 
-    //from the files array, find the checked radio button and return its value
-    for (var i = 0; i < files.length; i++) {
-        if (files[i].checked) {
-            radioValue = files[i].value;
-            
+
+function searchFile() {
+    radioValue = document.querySelector('.dropdown-toggle').value;
+    console.log(radioValue);
+    searchInput = document.getElementById('search-input').value;
+    fileData = JSON.parse(localStorage.getItem('files'));
+    //alert(searchInput.length);
+    
+
+    if(searchInput.length>0){
+        startSearch(fileData);
+    }
+    else{
+        alert("Please enter keyword to search!");
+    }
+}
+//Object to store search results if found
+var searchObject = new Object;
+    searchObject.title =[];
+    searchObject.author =[];
+    searchObject.location =[];
+    searchObject.size =[];
+    searchObject.date =[];
+    searchObject.description =[];
+    searchObject.href =[];
+
+function startSearch(event){
+    for(var i=0; i<fileData.length; i++){
+        for(var j=0; j<fileData[i].title.length; j++){
+            if((fileData[i].title[j].indexOf(searchInput)>-1)||(fileData[i].author[j].indexOf(searchInput)>-1)||(fileData[i].location[j].indexOf(searchInput)>-1)||(fileData[i].description[j].indexOf(searchInput)>-1)){
+                searchObject.title.push(fileData[i].title[j]);
+                searchObject.author.push(fileData[i].author[j]);
+                searchObject.location.push(fileData[i].location[j]);
+                searchObject.size.push(fileData[i].size[j]);
+                searchObject.date.push(fileData[i].date[j]);
+                searchObject.description.push(fileData[i].description[j]);
+                searchObject.href.push(fileData[i].href[j]);
+
+            }
         }
     }
-    displayData();
 
-}
-
-function displayData() {
-
-    if ((radioValue.length>0) && (searchInput.length>0)) {
-        //console.log('Search iput is = ' + searchInput);
-        //console.log('Radio value is ' + radioValue + ' and = ' + radioChecked);
+    if(searchObject.title.length>0){
+        alert(searchObject.title);
     }
-
     else{
-        alert('pick a search option');
+        alert("No files found matching "+'"'+searchInput+'"');
     }
+    
 }
